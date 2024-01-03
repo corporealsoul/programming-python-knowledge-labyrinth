0c84a495ed1aebaa2abfff8f1568e2f21242c657
ONOSENDAI\uniqs <bca.anup@gmail.com>
2024-01-03 12:44:26 +0530

    following-git-rules

---
+++ 20-apply-use-control/2024-python-examples/python-syntax.py
@@ -0,0 +1,100 @@
+# Comment
+# '''multiline
+# Comment'''
+# print("Restart !")
+
+
+
+# 1. Hello, World!
+# print("Restart ! ") #Comment
+
+
+
+# 2. Arithmetic Operations
+# first_int_var = int(input("Enter First Num : "))
+# second_int_var = int(input("Enter Second Num : "))
+# third_int_var = int(input("Enter Third Num : "))
+# print(first_int_var + second_int_var)
+# print(first_int_var - second_int_var)
+# print(first_int_var * second_int_var)
+# print(first_int_var / second_int_var)
+# print(first_int_var // second_int_var)
+# print(first_int_var % second_int_var)
+# print(first_int_var ** second_int_var)
+
+
+
+# 3. Variables and Data Types
+# string_var = "String"
+# print(type(string_var))
+# int_var = 30
+# print(type(int_var))
+# boolean_var = True
+# print(type(boolean_var))
+
+
+
+
+# 4. Lists
+# fruits = ["apple", "banana", "orange"]
+# print(fruits, type(fruits))
+# print(fruits[1])
+# fruits.append("grape")
+# print(fruits, type(fruits))
+
+
+
+# 5. Loops
+# for loop in range(5):
+#     print(loop)
+#
+# fruits = ["apple", "banana", "orange"]
+# for fruits_loop in fruits:
+#     print(fruits_loop)
+
+
+
+
+# 4. Conditional Statements (if-else)
+
+# voting_age = int(input("Enter age : "))
+#
+# if voting_age >= 18:
+#     print("You can VOTE")
+# else:
+#     print("You can't VOTE")
+
+
+
+# 5. Loops (for and while)
+# for loop in range (5):
+#     print(loop)
+
+# count = 0
+# while count < 5:
+#     print(count)
+#     count = count + 1
+
+
+# 6. Functions
+
+# def greet(name):
+#     print("Hello", name + "!")
+#
+# greet("Anup")
+# greet("Rishav")
+
+
+
+# 8. Dictionaries
+# person = {"name": "John", "age": 30, "city": "New York"}
+#
+# print(person)
+# print(person["name"])
+# print(person["age"])
+# print(person["city"])
+# person["email"] = "john@example.com"  # Adding new key-value pairs
+# print(person)
+
+
+
---
+++ 20-apply-use-control/2024-python-examples/python-variables.py
@@ -0,0 +1,64 @@
+# 1. Simple Assignment:
+# name = "Alice"
+# age = 30
+# print("Name : ", name)
+# print("Age : ", age)
+
+
+
+
+# 2. Reassignment:
+# int_var = 10
+# int_var = 20
+# print(int_var)
+
+
+
+
+# 3. Arithmetic Operations with Variables:
+# price = 19.99
+# quantity = 3
+# total_cost = price * quantity
+# print("Costs :", total_cost)
+
+
+
+
+# 4. Combining Variables with Strings:
+# greetings = "Welcome, "
+# username = "Python learner "
+# message = greetings + username + "!"
+# print(message)
+
+
+
+
+# 5. Using Variables in Input and Output:
+# name = input("Your Name : ")
+# print("Hello, " + name + "!")
+# print("Hello, ", name , "!")
+
+# int_var_ten = 10
+# int_var_twenty = 20
+# int_var_thirty = 30
+
+# print("Anup", "Kumar", "Mondal")
+# print(int_var_ten, int_var_twenty, int_var_thirty)
+# print(int_var_ten + int_var_twenty + int_var_thirty)
+
+
+
+
+
+# 6. Variables in Conditional Statements:
+# float_temperature = float(input("Enter temperature : "))
+# if float_temperature >= 30:
+#     print("Hot Day")
+# else:
+#     print("Cold day")
+
+
+
+# 7. Variables in Loops:
+for i in range(5):
+    print("Loop iteration:", i)
